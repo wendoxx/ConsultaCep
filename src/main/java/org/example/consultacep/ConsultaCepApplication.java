b61269de1c08789f91833b7b66e7390ffa1ab5f4
Wendel Silva <wendeldasilvamartins1952@gmail.com>
2024-08-16 20:48:51 -0300

    feat: add spring project

---
+++ src/main/java/org/example/consultacep/ConsultaCepApplication.java
@@ -0,0 +1,13 @@
+package org.example.consultacep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsultaCepApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsultaCepApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/consultacep/ConsultaCepApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.consultacep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultaCepApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
