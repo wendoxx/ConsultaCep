ea6849dcaef471d3f276e365ede94caaa853f7d0
Wendel Silva <wendeldasilvamartins1952@gmail.com>
2024-08-19 19:49:19 -0300

    feat: add controller

--- src/main/java/org/example/consultacep/controller/ConsultaCepController.java
+++ src/main/java/org/example/consultacep/controller/ConsultaCepController.java
@@ -1,7 +1,9 @@
-package org.example.consultacep;
+package org.example.consultacep.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.example.consultacep.dto.ConsultaCepDTO;
 import org.example.consultacep.service.ConsultaCepService;
+import org.slf4j.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,20 +12,22 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
-
+@Slf4j
 @RestController
 @RequestMapping("/consulta-cep")
 public class ConsultaCepController {
 
     ConsultaCepService consultaCepService = new ConsultaCepService();
-
+    Logger logger = org.slf4j.LoggerFactory.getLogger(ConsultaCepController.class);
     @GetMapping("/cep")
     public ResponseEntity<ConsultaCepDTO> getConsultaCep(@RequestParam String cep) {
         try {
             ConsultaCepDTO consultaCepDTO = consultaCepService.getConsultaCepDTO(cep);
-            return ResponseEntity.ok(consultaCepDTO); // Retorna o DTO como resposta da API
+            logger.info("Consulta realizada com sucesso");
+            return ResponseEntity.ok(consultaCepDTO);
         } catch (IOException | InterruptedException e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build(); // Retorna um erro 500 em caso de falha
+            logger.error("Erro ao realizar a consulta");
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
     }
 }
