79892e5019271ccb064eed85d0d6160ead189f57
Wendel Silva <wendeldasilvamartins1952@gmail.com>
2024-08-19 19:50:00 -0300

    feat: add dto

--- src/main/java/org/example/consultacep/dto/ConsultaCepDTO.java
+++ src/main/java/org/example/consultacep/dto/ConsultaCepDTO.java
@@ -1,4 +1,39 @@
 package org.example.consultacep.dto;
 
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class ConsultaCepDTO {
+
+    private String cep;
+    private String logradouro;
+    private String complemento;
+    private String unidade;
+    private String bairro;
+    private String localidade;
+    private String uf;
+    private String ibge;
+    private String gia;
+    private String ddd;
+    private String siafi;
+
+    @Override
+    public String toString() {
+        return "ConsultaCepDTO{" +
+                "cep='" + cep + '\'' +
+                ", logradouro='" + logradouro + '\'' +
+                ", complemento='" + complemento + '\'' +
+                ", unidade='" + unidade + '\'' +
+                ", bairro='" + bairro + '\'' +
+                ", localidade='" + localidade + '\'' +
+                ", uf='" + uf + '\'' +
+                ", ibge='" + ibge + '\'' +
+                ", gia='" + gia + '\'' +
+                ", ddd='" + ddd + '\'' +
+                ", siafi='" + siafi + '\'' +
+                '}';
+    }
+
 }
